b559c3b2d91d62306cb8a3926227b56603201c72
Uranus-s <31566159+Uranus-s@users.noreply.github.com>
2021-10-10 22:44:17 +0800

    初始化

---
+++ src/main/java/com/example/springbootautotest/SpringbootAutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootautotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAutoTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootautotest/SpringbootAutoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootautotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
